d332fdc6b495f238f13e680a786366e8216decc8
Jara <143405392+jaaza9995@users.noreply.github.com>
2025-10-08 12:35:26 +0200

    ordercontroller

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -18,4 +18,42 @@ public class OrderController : Controller
         List<Order> orders = await _itemDbContext.Orders.ToListAsync();
         return View(orders);
     }
+
+    [HttpGet]
+    public IActionResult CreateOrderItem()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateOrderItem(OrderItem orderItem)
+    {
+        try
+        {
+            var newItem = _itemDbContext.Items.Find(orderItem.ItemId);
+            var newOrder = _itemDbContext.Orders.Find(orderItem.OrderId);
+
+            if (newItem == null || newOrder == null)
+            {
+                return BadRequest("Item or Order not found");
+            }
+
+            var newOrderItem = new OrderItem
+            {
+                ItemId = orderItem.ItemId,
+                Item = newItem,
+                Quantity = orderItem.Quantity,
+                Order = newOrder,
+            };
+            newOrderItem.OrderItemPrice = orderItem.Quantity * newOrderItem.Item.Price;
+
+            _itemDbContext.OrderItems.Add(newOrderItem);
+            await _itemDbContext.SaveChangesAsync();
+            return RedirectToAction(nameof(Table));
+        }
+        catch
+        {
+            return BadRequest("OrderItem creation failed");
+        }
+    }
 }
